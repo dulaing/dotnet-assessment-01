ed98bb628f545ccb77b0517214049bc81e22b103
dulaing <dulain.gunawardhana@ascentic.se>
2026-08-19 13:34:49 +0530

    feat: add library db context

---
+++ src/Library.Infrastructure/Persistence/LibraryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Infrastructure.Persistence
+{
+    // the single object that knows our tables and talks to Postgres
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+        }
+
+        // 3 tables
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Member> Members => Set<Member>();
+        public DbSet<Borrowing> Borrowings => Set<Borrowing>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // this tells EF to look through this project, find every config class I wrote, apply them all.
+            // without this line, EF guesses everything and it makes every string an unlimited-length coumn and adds no unique constraints
+            // thats why we write config classes
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LibraryDbContext).Assembly);
+        }
+    }
+}
